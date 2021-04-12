cadbd288d04080c938ce3f80a954747b4b86c97b
Tato9591 <marco.schenardi@alice.it>
2021-04-12 14:05:25 +0200

    Add files via upload

--- hwmonitor.py
+++ hwmonitor.py
@@ -157,7 +157,12 @@ while True:
         draw.text((x+76, top+13), chr(63587), font=font_icon, fill=255)
     if GPIO.input(14) == False: # pin state control = OFF
         # Icon Temp (62154) temperature state is OK
-        draw.text((x+85, top+13), chr(62154), font=font_icon2, fill=255)
+        if Temp >= 65:
+            draw.text((x+85, top+13), chr(62152), font=font_icon2, fill=255)
+        if Temp > 40:
+            draw.text((x+85, top+13), chr(62153), font=font_icon2, fill=255)
+        if Temp <= 40:
+            draw.text((x+85, top+13), chr(62154), font=font_icon2, fill=255)
 
     # Display image
     disp.image(image)
