c319aba99b70f5990b85f03baa5a1cc39e3ae47e
Tato9591 <marco.schenardi@alice.it>
2021-05-24 15:36:07 +0200

    Add files via upload

--- hwmonitor.py
+++ hwmonitor.py
@@ -7,6 +7,7 @@
 # not support PIL/pillow (python imaging library)!
 from vcgencmd import Vcgencmd
 #Per documentazione vedi https://pypi.org/project/vcgencmd/
+from pathlib import Path
 from mail import send_email # inserire i dati in mail.py
 import time
 import os
@@ -24,6 +25,12 @@ def get_temp():
     temp = vcgm.measure_temp()
     return(temp)
 
+def get_ora():
+    ora=time.strftime("%H")
+    minuti=time.strftime("%M")
+    orario=ora+":"+minuti
+    return(orario)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -39,7 +46,7 @@ GPIO.setwarnings(False)
 #|-----------------------------------------------------------------------------|
 #|digitare:                                                                    |
 #|sudo nano /boot/config.txt                                                   |
-#|modificare il valore temp=80000 in (temp=46000) o a piacere.                              |
+#|modificare il valore temp=80000 in (temp=46000) o a piacere.                 |
 #|> [all]                                                                      |
 #|> dtoverlay=gpio-fan,gpiopin=14,temp=46000                                   |
 #|                                                                             |
@@ -80,8 +87,6 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-posta = 0
-
 # Load default font.
 font = ImageFont.load_default()
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
@@ -94,14 +99,81 @@ font_icon3 = ImageFont.truetype('fa-brands-400.ttf', 26)
 font_icon4 = ImageFont.truetype('fa-solid-900.ttf', 8)
 #font_text_small = ImageFont.truetype('Montserrat-Medium.ttf', 8)
 
-posta = 0
+posta = 0 # variabile per segnalare invio mail
+Cont = 0  # variabile per decidere scrittura giornaliera su FAN.txt
+inizioDay = "00:00" # inizio giornata
+fineDay = "23:59"   # fine giornata
+ContFAN = 0 # variabile conteggio accensioni ventola
+ValPinFAN = 0 # variabile controllo pin accensione ventola
+
+if Path('ENEL.txt').is_file(): # controllo esistenza file
+    f = open("ENEL.txt","r+")  # se esiste recupero dato dopo reboot
+    for line in f:
+        val = line.strip()
+        Cont = int(val) # variabile controllo per scrivere su FAN.txt
+    f.close()
+    time.sleep(0.5)
+else:
+    writepath = 'ENEL.txt' # se non esiste lo creo
+    mode = 'a' if os.path.exists(writepath) else 'w+'
+    with open(writepath, mode) as f:
+        f.write(str(Cont)) # scrivo valore
+        f.close()
+        time.sleep(0.5)
+if Path('FanBkp.txt').is_file(): # controllo esistenza file
+    f = open("FanBkp.txt","r+")  # se esiste recupero dato dopo reboot
+    for line in f:
+        val = line.strip()
+        ContFAN = int(val) # variabile conteggio accensioni ventola
+    f.close()
+    time.sleep(0.5)
+else:
+    writepath = 'FanBkp.txt' # se non esiste lo creo
+    mode = 'a' if os.path.exists(writepath) else 'w+'
+    with open(writepath, mode) as f:
+        f.write(str(ContFAN)) # scrivo il valore
+        f.close()
+        time.sleep(0.5)
 
 while True:
     
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-    # Controllo funzionamento ventola
+    
+    # registrazione su file FAN.txt delle attivazioni della ventola at Day
+    Ora = get_ora()
+    Day = time.ctime()
+    writepath = 'FAN.txt'
+    mode = 'a' if os.path.exists(writepath) else 'w'
+    with open(writepath, mode) as f:
+        if os.stat("FAN.txt").st_size == 0: # file vuoto
+            f.write(Day) # scrittura prima volta oltre le ore 00:00
+            f.close()                       
+            time.sleep(0.5)
+            Cont += 1
+        if Ora == inizioDay and Cont == 0:
+            Cont += 1
+            f.write(Day)
+            f.close()
+            time.sleep(0.5)
+        if Ora == fineDay and Cont == 1:
+            Cont = 0
+            testo=" FAN si è attivata " +str(ContFAN)+ " volte.\n"
+            f.write(testo)
+            f.close()
+            time.sleep(0.5)
+            ContFAN = 0 # azzeramento contatore accensioni ventola
+            with open("FanBkp.txt","w+") as S:
+                S.write(str(ContFAN))
+                S.close()
+                time.sleep(0.5)
+        with open("ENEL.txt","w+") as S:
+            val = Cont
+            S.write(str(val))
+            S.close()
+            time.sleep(0.5)
+            
+    # Controllo integrità della ventola.
     Temp = get_temp()
     if Temp >= 70: # Probabilmente la ventola è fuori uso mi spengo.
         draw.text((x, top+6), "Shutdown", font=font1, fill=255)
@@ -114,7 +186,7 @@ while True:
         time.sleep(3)
         os.system("sudo shutdown -h now")
         break
-    if Temp >= 50 and posta <= 0:
+    if Temp >= 50 and posta == 0: # primo avviso con mail.
         posta += 1
         send_email()
         time.sleep(0.5)
@@ -139,7 +211,7 @@ while True:
     # Text IP address
     draw.text((x+30, top), str(IP), font=font, fill=255)
     # Text Temperature CPU
-    draw.text((x+98, top+13), str(Temp) + "°", font=font1, fill=255)
+    draw.text((x+98, top+11), str(Temp) + "°", font=font1, fill=255)
     # Text CPU
     draw.text((x, top+9), str(CPU), font=font, fill=255)
     # Text HDD used/total
@@ -153,16 +225,33 @@ while True:
         # Icon Mail (61664) inviato email di avviso poi mi spengo.
         draw.text((x, top+2), chr(61664), font=font_icon4, fill=255)
     if GPIO.input(14) == True: # pin state control = ON
+        if ValPinFAN == 0:
+            ContFAN += 1
+            with open("FanBkp.txt","w+") as f:
+                f.write(str(ContFAN)) # backup variabile dopo reboot
+                f.close()
+                time.sleep(0.5)
+            ValPinFAN = 1
         # Icon FAN (63587) ignition confirmation
         draw.text((x+76, top+13), chr(63587), font=font_icon, fill=255)
     if GPIO.input(14) == False: # pin state control = OFF
+        draw.text((x+98,top+27), chr(63587), font=font_icon4, fill=255)
+        ValPinFAN = 0
         # Icon Temp (62154) temperature state is OK
-        if Temp > 60:
+        if Temp > 60 and Temp < 80:
             draw.text((x+85, top+13), chr(62152), font=font_icon2, fill=255)
         if Temp > 40 and Temp <= 60:
             draw.text((x+85, top+13), chr(62153), font=font_icon2, fill=255)
         if Temp <= 40:
             draw.text((x+85, top+13), chr(62154), font=font_icon2, fill=255)
+        # visualizza conteggio accensione giornaliero del FAN.
+        if ContFAN < 10:
+            draw.text((x+98,top+24), "  00"+str(ContFAN), font=font, fill=255)
+        if ContFAN < 100:
+            draw.text((x+98,top+24), "  0"+str(ContFAN), font=font, fill=255)
+        if ContFAN >= 100:
+            draw.text((x+98,top+24), "  "+str(ContFAN), font=font, fill=255) 
+        
 
     # Display image
     disp.image(image)
